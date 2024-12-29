f6c814a7018f3d5220aa14317df16b028a0b72bf
Salhi-Abdennour <salhiabdennour3@gmail.com>
2024-12-29 14:29:02 +0100

    Update server.js

--- Server/server.js
+++ Server/server.js
@@ -53,8 +53,3 @@ process.on('unhandledRejection', (reason, promise) => {
 app.listen(3001, '0.0.0.0', () => {
   console.log('Server running on http://0.0.0.0:3001');
 });
-
-// JANDOUBA
-// LEE MA8dartnich
-// 3asba 8dartek
-//test conflects 2
